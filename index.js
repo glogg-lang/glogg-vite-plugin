5213eebd9083b1668e492af7f411174813def4dc
Robin Heggelund Hansen <git@heghan.org>
2025-02-25 11:47:25 +0100

    Format code.

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import proc from "node:child_process";
 import url from "node:url";
 import * as glogg from "glogg-lang";
 
-const fileRegex = /\.glogg\.db$/
+const fileRegex = /\.glogg\.db$/;
 
 export default function plugin() {
   return {
-    name: 'glogg',
+    name: "glogg",
 
     async load(id) {
       if (fileRegex.test(id)) {
         return await glogg.make(id);
       }
     },
-  }
+  };
 }
