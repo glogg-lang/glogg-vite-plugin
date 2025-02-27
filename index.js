f25ad3f397c51fe89a6323a00df0eaaa0a637890
Robin Heggelund Hansen <git@heghan.org>
2025-02-27 22:01:20 +0100

    Use new integration API.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import fs from "node:fs";
 import proc from "node:child_process";
 import url from "node:url";
-
-// TODO: glogg-lang should probably expose an API to avoid spawning child process
-const glg = url.fileURLToPath(import.meta.resolve("glogg-lang/bin"));
+import * as glogg from "glogg-lang";
 
 const fileRegex = /\.glogg\.db$/
 
@@ -11,17 +9,9 @@ export default function plugin() {
   return {
     name: 'glogg',
 
-    transform(src, id) {
+    async load(id) {
       if (fileRegex.test(id)) {
-        // TODO: make should support outputting code to stdout
-        proc.execSync(`node ${glg} make`);
-
-        const code = fs.readFileSync("app.js", { encoding: "utf-8" });
-
-        return {
-          code: code,
-          map: null, // provide source map if available
-        }
+        return await glogg.make(id);
       }
     },
   }
