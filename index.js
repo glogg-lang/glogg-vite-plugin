01c98a133f399c2cde0126ba4428b9994da18a91
Robin Heggelund Hansen <git@heghan.org>
2025-02-28 11:02:15 +0100

    Re-brand as rollup plugin

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import fs from "node:fs";
-import proc from "node:child_process";
-import url from "node:url";
 import * as glogg from "glogg-lang";
 
 const fileRegex = /\.glogg\.db$/;
 
 export default function plugin() {
   return {
-    name: "glogg",
+    name: "rollup-plugin-glogg",
 
     async load(id) {
       if (fileRegex.test(id)) {
